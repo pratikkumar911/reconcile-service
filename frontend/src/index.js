53bb6723368ad675659302e09a79a286405e1d94
Pratik <kumar.pratik42000@gmail.com>
2026-07-20 22:18:16 +0530

    fixed deployment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 60_000,
-      refetchOnWindowFocus: false,
-    },
-  },
-});
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
